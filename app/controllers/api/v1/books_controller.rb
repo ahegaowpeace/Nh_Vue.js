6d4e3c1baed423d02e19700059a1300219d30d2c
ahegaowpeace <arduino0202@gmail.com>
2020-08-05 03:42:08 +0900

    Add Controller For PageNation

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,18 @@ class Api::V1::BooksController < ApiController
   end
 
 	def index
-		books = Book.all
+		# 現ページ
+		nowpage = params[:page].to_i
+		# タイトル数
+		booksnum = Book.count
+		# 表示数
+		viewnum = 10
+		# 表示するid範囲
+		startid = booksnum - ((nowpage - 1) * viewnum)
+		lastid = startid - viewnum + 1
+		lastid = 1 if lastid < 0
+
+		books = Book.all.order(id: :desc).where("(id <= ?) AND (id >= ?)", startid, lastid)
     render json: books
   end
 end
