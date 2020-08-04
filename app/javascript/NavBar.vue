751d63a6095bdc5c5a6a933057bb5184ad8c21eb
ahegaowpeace <arduino0202@gmail.com>
2020-08-04 12:11:45 +0900

    Add Bootstap

--- app/javascript/NavBar.vue
+++ app/javascript/NavBar.vue
@@ -1,16 +1,29 @@
 <template>
-	<header>
-		<nav>
-			<ul>
-				<li>hoge</li>
-				<li>moge</li>
-			</ul>
-		</nav>
-	</header>
+	<div class="container">
+		<a href="#">menu</a>
+		<ul class="col-sm-4">
+			<li>hoge</li>
+			<li>moge</li>
+		</ul>
+	<b-button v-b-tooltip.hover title="Tooltip content">Hover Me</b-button>
+	</div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+.menu {
+	width: 100%;
+	max-width: 960px;
+	margin: 0 auto;
+	padding: 0;
+}
+
+.menu li {
+	display: block;
+	float: left;
+	width: 20%;
+	padding: 0;
+}
 </style scoped>
